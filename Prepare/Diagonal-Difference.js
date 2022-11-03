129e42eda06dbb2789fadc97724bb41d154dc5b4
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-11-03 06:07:45 -0300

    Create Diagonal-Difference.js

---
+++ Prepare/Diagonal-Difference.js
@@ -0,0 +1,17 @@
+function fiagonalDifference(arr){
+  let n = are.length
+  let diag1 = 0
+  let diag2 = 0
+
+  for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+      if(i === j){
+        diag1 += arr[i][j]
+      }
+      if(i + j === n - 1){
+        diag2 += arr[i][j]
+      }
+    }
+  }
+  return Math.abs(diag1 - diag2)
+}
