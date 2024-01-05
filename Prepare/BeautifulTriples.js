0cb31fdfa564b3ed2c62bea4b602c5388732583d
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-01-05 21:06:53 -0200

    Create BeautifulTriples.js

---
+++ Prepare/BeautifulTriples.js
@@ -0,0 +1,10 @@
+function beautifulTripletd(d,arr){
+  let count = 0
+  
+  for(let i = 0; i < arr.length; i++){
+    if(arr.includes(arr[i] + d) && arr.includes(arr[i] + 2 * d)){
+      count += 1
+    }
+  }
+  return count
+}
