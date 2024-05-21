41f15c2d17e36d6612bccfd28304486654842b75
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-05-21 09:00:35 -0300

    Update BetweenTwoSets.js

--- Prepare/BetweenTwoSets.js
+++ Prepare/BetweenTwoSets.js
@@ -1,12 +1,25 @@
-function getTotalX(a, b) {
-  let validCount = 0
-  
-  for(let x = 1; x <= 100; x++){
-    if(a.every(int => (x % int == 0))){
-      if(b.every(int => (int % x == 0))){
-        validCount += 1
-      }
+function getTotalX(a,b){
+  function gcd(x,y){
+    while(y){
+      [x,y] = [y, x % y]
+    }
+    return x;
+  }
+
+  function lcm(x,y){
+    return (x * y) / gcd(x,y);
+  }
+
+  let lcm_a = a.reduce((acc,val) => lcm(acc,val), 1);
+  let gcd_b = b.reduce((acc,val) => gcd(acc,val));
+
+  let count = 0;
+
+  for(let multiple = lcm_a; multiple <= gcd_b; multiple += lcm_a){
+    if(gcd_b % multiple === 0){
+      count++
     }
   }
-  return validCount
+
+  return count;
 }
